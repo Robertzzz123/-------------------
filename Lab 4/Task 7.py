662a3588bbc7b6d9e2eef0558be8ca5708ca1036
Robertzzz123 <60054625+Robertzzz123@users.noreply.github.com>
2020-12-13 21:22:09 +0300

    Update Task 7.py

--- Lab 4/Task 7.py
+++ Lab 4/Task 7.py
@@ -1,33 +1,20 @@
-def BubbleSort(array):
-    i = 0
-    j = 1
-    arr = array.copy()
-    while j < len(arr):
-        i = 0
-        while i < len(arr)-1:
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-            i = i + 1
-        j = j + 1
-    return arr
+mas = [7, 7, 7, 7, 7, 7]
+maximum = mas[0]
+for i in range(1, len(mas)):
+    if mas[i] > maximum:
+            maximum = mas[i]
 
-def SecondMax(array):
-    IsEnd = False
-    arr = BubbleSort(array)
-    i = len(arr) - 1
-    while not IsEnd:
-        if i < 1:
-            IsEnd = True
-            firstMax = i
-        elif arr[i] > arr[i-1]:
-            IsEnd = True
-            firstMax = i
-        i = i - 1
-    if firstMax > 0 and arr[firstMax] > arr[firstMax-1]:
-        secondMax = "Второй максимум: " + str(arr[firstMax-1])
-    else:
-        secondMax = "Второго максимума нет"
-    return secondMax
-
-arr = [5,5,5,5,5,5,5]
-print(SecondMax(arr))
+maximums = maximum
+mas1 = mas
+for i in range(1, len(mas1)):
+    if mas1[i] < maximum  :
+        maximums = mas1[i]
+if maximums == maximum:
+    print ("No")
+else:
+  
+    for i in range(0, len(mas1)):
+         if mas[i] > maximums and  mas1[i] < maximum  :
+            maximums = mas1[i]
+            
+    print (maximums)
